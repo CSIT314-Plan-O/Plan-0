cc0c2b949ed23c2189bf557b8f0ed08cc25a771e
fidellim <lim.fidel@gmail.com>
2019-11-23 18:06:46 +0400

    add menu

--- app/src/main/java/com/example/myapplication/Menu.java
+++ app/src/main/java/com/example/myapplication/Menu.java
@@ -1,21 +1,30 @@
 package com.example.myapplication;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+public class Menu extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-public class Menu extends AppCompatActivity {
-
-    Button signOut;
+    NavigationView navigationView;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
     FirebaseAuth mFirebaseAuth;
     FirebaseAuth.AuthStateListener mAuthListener;
     private GoogleSignInClient mGoogleSignInClient;
@@ -25,7 +34,20 @@ public class Menu extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        signOut = (Button) findViewById(R.id.btnSignOut);
+
+        navigationView = findViewById(R.id.nav_view);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(this);
+
         mFirebaseAuth = FirebaseAuth.getInstance();
         GoogleSignInOptions gso = new GoogleSignInOptions
                 .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -45,21 +67,52 @@ public class Menu extends AppCompatActivity {
             }
         };
 
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                signOut();
-            }
-        });
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ReminderFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_reminder);
+        }
+
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_reminder:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ReminderFragment()).commit();
+                break;
+            case R.id.nav_task:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TaskFragment()).commit();
+                break;
+            case R.id.nav_pomodoro:
+                startActivity(new Intent(this, Pomodoro.class));
+                break;
+            case R.id.nav_logout:
+                mFirebaseAuth.signOut();
+                mGoogleSignInClient.signOut();
+                break;
+        }
+
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
     protected void onStart(){
         super.onStart();
         mFirebaseAuth.addAuthStateListener(mAuthListener);
     }
 
-    private void signOut(){
-        mFirebaseAuth.signOut();
-        mGoogleSignInClient.signOut();
+    @Override
+    public void onBackPressed(){
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
+
     }
+
 }
