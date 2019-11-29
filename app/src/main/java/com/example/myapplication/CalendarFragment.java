2c07956454191ee1d78e51ae419eb49ee6eed050
fidellim <lim.fidel@gmail.com>
2019-11-30 03:38:27 +0400

    add calendarView

--- app/src/main/java/com/example/myapplication/CalendarFragment.java
+++ app/src/main/java/com/example/myapplication/CalendarFragment.java
@@ -9,11 +9,33 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.applandeo.materialcalendarview.CalendarView;
+import com.applandeo.materialcalendarview.exceptions.OutOfDateRangeException;
+
+import java.util.Calendar;
+
 public class CalendarFragment extends Fragment {
+
+    CalendarView calendarView;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_calendar, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(2019, 11, 2);
+
+        calendarView = (CalendarView) getView().findViewById(R.id.calendarView);
+        try {
+            calendarView.setDate(calendar);
+        } catch (OutOfDateRangeException e) {
+            e.printStackTrace();
+        }
     }
 }
