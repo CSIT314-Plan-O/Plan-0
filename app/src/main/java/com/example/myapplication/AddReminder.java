9fbeb079d967debe4b9094c7b7ab8b3a03cd9877
fidellim <lim.fidel@gmail.com>
2019-12-06 19:28:06 +0400

    add menu icons

--- app/src/main/java/com/example/myapplication/AddReminder.java
+++ app/src/main/java/com/example/myapplication/AddReminder.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class AddReminder extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class AddReminder extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_reminder);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.add_todo, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.submit_button:
+
+                finish();
+                return true;
+
+            case R.id.cancel_button:
+                setResult(RESULT_CANCELED, null);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
