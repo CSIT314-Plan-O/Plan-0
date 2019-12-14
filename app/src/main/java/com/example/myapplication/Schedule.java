63d8aeae0a62654fa0927e1fc6415768709310a3
fidellim <lim.fidel@gmail.com>
2019-12-14 19:35:29 +0400

    add schedule

--- app/src/main/java/com/example/myapplication/Schedule.java
+++ app/src/main/java/com/example/myapplication/Schedule.java
@@ -1,19 +1,60 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class Schedule extends AppCompatActivity {
+
+    private static final String KEY_CLASS = "Class";
+    private static final String KEY_TIMING = "Timing";
+
+    private static final String TAG = "-----------------------";
+
     Toolbar toolbar;
+    ToDoListAdapterForSchedule mAdapter;
+    ListView listView;
+
+    private FirebaseFirestore db;
+    CollectionReference academicClassesUsers;
+    FirebaseAuth firebaseAuth;
+    String userId;
+    FirebaseUser firebaseUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+        if(firebaseUser != null){
+            userId = firebaseUser.getUid();
+            academicClassesUsers = db.collection("Users").document(userId).collection("AcademicClasses");
+            //getDatabase();
+        }
+
+        listView = findViewById(R.id.listview);
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -31,5 +72,41 @@ public class Schedule extends AppCompatActivity {
                 finish();
             }
         });
+
+        mAdapter = new ToDoListAdapterForSchedule(this);
+        listView.setAdapter(mAdapter);
+    }
+
+    //Get toDoItem from database
+    public void getDatabase(){
+        academicClassesUsers.get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                if(!queryDocumentSnapshots.isEmpty()){
+                    List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+
+                    for(DocumentSnapshot d : list){
+
+                        String academicClass = d.getString(KEY_CLASS);
+                        String timing = d.getString(KEY_TIMING);
+
+                        mAdapter.add(new AcademicClass(academicClass, timing));
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "INSERTION OF DATA IS SUCCESS");
+            }
+        });
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        getDatabase();
     }
 }
